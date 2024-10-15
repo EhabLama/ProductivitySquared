380b14f8f02dfb31a57b508dba237e803a2cc3a8
Ehab <ehab_alaama@hotmail.com>
2024-10-15 19:40:34 +0800

    SPFx project setup, basic web part setup

---
+++ src/webparts/productivityOverview/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IProductivityOverviewWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ProductivityOverviewWebPartStrings' {
+  const strings: IProductivityOverviewWebPartStrings;
+  export = strings;
+}
